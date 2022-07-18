0b0037cb2f22ea28beefa7c4601502cbd45734ce
coderodyhpc <79646720+coderodyhpc@users.noreply.github.com>
2022-07-18 12:54:11 -0400

    Update mainPlugin.py

--- plugin/mainPlugin.py
+++ plugin/mainPlugin.py
@@ -40,6 +40,7 @@ from Gv3GEWRF.core import (logger)
 ###    ADD_BINARY_LAYER_ICON_PATH, ABOUT_ICON_PATH, BUG_ICON_PATH)
 from Gv3GEWRF.plugin.constants import (PLUGIN_NAME)
 
+from . import resources
 
 class QGISPlugin():
     def __init__(self, iface: QgisInterface) -> None:
