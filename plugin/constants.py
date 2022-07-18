4bc0f8690c2fbd6b4675e42c048fa1aaea81a9fb
coderodyhpc <79646720+coderodyhpc@users.noreply.github.com>
2022-07-18 12:18:22 -0400

    Update constants.py

--- plugin/constants.py
+++ plugin/constants.py
@@ -5,7 +5,7 @@
 # interferes with bootstrapping.
 
 PLUGIN_NAME = 'Gv3GEWRF'
-ABOUT_ICON_PATH = ':/resources/logo512.png'
+ABOUT_ICON_PATH = ':/resources/icon512.png'
 ###GIS4WRF_LOGO_PATH = ':/plugins/Gv3GEWRF/gis4wrf_logo.svg'
 ###BUG_ICON_PATH = ':/plugins/Gv3GEWRF/bug_icon.svg'
 ###ADD_WRF_NETCDF_LAYER_ICON_PATH = ':/plugins/Gv3GEWRF/AddWRFNetCDFLayer.svg'
