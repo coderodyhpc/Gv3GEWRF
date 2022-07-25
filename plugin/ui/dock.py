b63f9ad8887a745f95549747fc09f243fc27a1b4
coderodyhpc <79646720+coderodyhpc@users.noreply.github.com>
2022-07-25 18:06:19 -0400

    Update dock.py

--- plugin/ui/dock.py
+++ plugin/ui/dock.py
@@ -6,9 +6,9 @@ from PyQt5.QtWidgets import QDockWidget, QTabWidget
 from qgis.gui import QgisInterface
 
 from Gv3GEWRF.plugin.ui.tab_home import HomeTab
-###from Gv3GEWRF.plugin.ui.tab_datasets import DatasetsTab
+from Gv3GEWRF.plugin.ui.tab_datasets import DatasetsTab
 from Gv3GEWRF.plugin.ui.tab_simulation import SimulationTab
-###from Gv3GEWRF.plugin.ui.widget_view import ViewWidget
+from Gv3GEWRF.plugin.ui.widget_view import ViewWidget
 from Gv3GEWRF.plugin.ui.helpers import WhiteScroll
 
 class MainDock(QDockWidget):
@@ -18,11 +18,11 @@ class MainDock(QDockWidget):
 
         tabs = QTabWidget()
         tabs.addTab(WhiteScroll(HomeTab()), 'Home')
-###        tabs.addTab(DatasetsTab(iface), "Datasets")
-###        self.simulation_tab = SimulationTab(iface)
+        tabs.addTab(DatasetsTab(iface), "Datasets")
+        self.simulation_tab = SimulationTab(iface)
         tabs.addTab(self.simulation_tab, "Simulation")
-###        self.view_tab = ViewWidget(iface, dock_widget)
-###        tabs.addTab(self.view_tab, "View")
+        self.view_tab = ViewWidget(iface, dock_widget)
+        tabs.addTab(self.view_tab, "View")
         self.setWidget(tabs)
         self.tabs = tabs
 
