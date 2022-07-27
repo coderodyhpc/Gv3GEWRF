2f172dc55106d3752d4e74388c434f49fb0e607e
coderodyhpc <79646720+coderodyhpc@users.noreply.github.com>
2022-07-27 07:05:16 -0400

    Update constants.py

--- plugin/constants.py
+++ plugin/constants.py
@@ -1,14 +1,11 @@
 # Gv3GEWRF Plugin
 # Copyright (c) 2022 Odycloud.
 
-# IMPORTANT: Do not import anything from gis4wrf.core here as this
-# interferes with bootstrapping.
 
 PLUGIN_NAME = 'Gv3GEWRF'
 ###ABOUT_ICON_PATH = ':/resources/icon512.png'
-###GIS4WRF_LOGO_PATH = ':/plugins/Gv3GEWRF/gis4wrf_logo.svg'
+###Gv3GEWRF_LOGO_PATH = ':/plugins/Gv3GEWRF/Gv3GEWRF_logo.svg'
 ###BUG_ICON_PATH = ':/plugins/Gv3GEWRF/bug_icon.svg'
 ###ADD_WRF_NETCDF_LAYER_ICON_PATH = ':/plugins/Gv3GEWRF/AddWRFNetCDFLayer.svg'
 ###ADD_BINARY_LAYER_ICON_PATH = ':/plugins/Gv3GEWRF/AddBinaryLayer.svg'
 
-###MSMPI_DOWNLOAD_PAGE = 'https://www.microsoft.com/en-us/download/details.aspx?id=56727'
