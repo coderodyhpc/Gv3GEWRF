abdc61992a16001151df88c62098f8633ee2ee19
coderodyhpc <79646720+coderodyhpc@users.noreply.github.com>
2022-08-01 19:18:51 -0400

    Create widget_geogrid.py

---
+++ plugin/ui/widget_geogrid.py
@@ -0,0 +1,98 @@
+# Gv3GEWRF 
+# Copyright (c) Odycloud.
+
+from pathlib import Path
+
+from PyQt5.QtCore import Qt
+from PyQt5.QtCore import pyqtSignal
+from PyQt5.QtWidgets import (
+    QWidget, QTabWidget, QPushButton, QLayout, QVBoxLayout, QDialog, QGridLayout, QGroupBox, QSpinBox,
+    QLabel, QHBoxLayout, QComboBox, QScrollArea, QFileDialog, QRadioButton, QLineEdit, QTableWidget,
+    QTableWidgetItem, QTreeWidget, QTreeWidgetItem
+)
+from PyQt5.QtGui import QPixmap
+
+from Gv3GEWRF.core import Project
+from Gv3GEWRF.plugin.options import get_options
+###from Gv3GEWRF.plugin.constants import Gv3GEWRF_LOGO_PATH
+
+class GeogridWidget(QWidget):
+    create_project = pyqtSignal(str)
+    open_project = pyqtSignal(str)
+    close_project = pyqtSignal()
+    tab_active = pyqtSignal()
+
+    def __init__(self) -> None:
+        super().__init__()
+
+        self.options = get_options()
+
+        btn_new = QPushButton("Create Project",
+            clicked=self.on_create_project_button_clicked)
+
+        btn_existing = QPushButton("Open Project",
+            clicked=self.on_open_project_button_clicked)
+
+        self.current_project_label = QLabel()
+
+        vbox = QVBoxLayout()
+        title = """
+                    <html>
+                        <h2>Preprocessing Tools</h2>
+                        <br>
+                    </html>
+                """
+        text = """
+                    <html>
+                        <br>
+                        <h2>geogrid.</h2>
+                  </html>
+               """
+
+        label_title = QLabel(title)
+        label_text = QLabel(text)
+        label_text.setWordWrap(True)
+        label_text.setOpenExternalLinks(True)
+#        label_pixmap = QLabel()
+#        pixmap = QPixmap('/home/ubuntu/.local/share/QGIS/QGIS3/profiles/default/python/plugins/Gv3GEWRF/plugin/resources/icon512.png')
+#        label_pixmap.setPixmap(pixmap)
+#        label_pixmap.setAlignment(Qt.AlignCenter)
+        vbox.addWidget(label_title)
+#        vbox.addWidget(label_pixmap)
+        vbox.addWidget(label_text)
+        vbox.addStretch()
+
+        vbox.addWidget(btn_new)
+        vbox.addWidget(btn_existing)
+        vbox.addWidget(self.current_project_label)
+        self.setLayout(vbox)
+
+    @property
+    def project(self) -> Project:
+        return self._project
+
+    @project.setter
+    def project(self, val: Project) -> None:
+        ''' Sets the currently active project. See tab_simulation. '''
+        self._project = val
+        self.update_project_path_label()
+
+    def on_create_project_button_clicked(self):
+        folder = QFileDialog.getExistingDirectory(
+            caption='Select new project folder', directory=self.options.projects_dir)
+        if not folder:
+            return
+        self.create_project.emit(folder)
+
+    def on_open_project_button_clicked(self):
+        folder = QFileDialog.getExistingDirectory(
+            caption='Select existing project folder', directory=self.options.projects_dir)
+        if not folder:
+            return
+        self.open_project.emit(folder)
+
+    def update_project_path_label(self) -> None:
+        path = self.project.path
+        if path is None:
+            path = 'N/A'
+        self.current_project_label.setText('Project path: ' + path)
