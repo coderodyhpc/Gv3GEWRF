a37c00540d2d62f129029abec41ff025f2fd4158
coderodyhpc <79646720+coderodyhpc@users.noreply.github.com>
2022-08-01 14:45:41 -0400

    Update tab_WRF.py

--- plugin/ui/tab_WRF.py
+++ plugin/ui/tab_WRF.py
@@ -39,7 +39,7 @@ class WRFTab(QTabWidget):
         self.datasets_tab.go_to_run_tab.connect(self.open_run_tab)
         self.run_tab.view_wrf_nc_file.connect(self.view_wrf_nc_file)
 
-        self.addTab(WhiteScroll(self.general_tab), 'General')
+        self.addTab(WhiteScroll(self.general_tab), 'Description')
         self.addTab(WhiteScroll(self.domain_tab), 'Domain')
         self.addTab(WhiteScroll(self.datasets_tab), 'Data')
         self.addTab(WhiteScroll(self.run_tab), 'Run')
