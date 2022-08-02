4674388c0cb3e9a96639fa513d5a94368195ef99
coderodyhpc <79646720+coderodyhpc@users.noreply.github.com>
2022-08-02 11:59:31 -0400

    Update widget_vanilla.py

--- plugin/ui/widget_vanilla.py
+++ plugin/ui/widget_vanilla.py
@@ -104,6 +104,9 @@ class VanillaWidget(QWidget):
                          supports_mpi=False)
 
     def prepare_wps_run(self) -> None:
+        print("At prepare_wps_run ")
+        aaa=self.options.wps_dir
+        print(aaa)
         if not self.options.wps_dir:
             raise WPSDistributionError('WPS is not setup')
         self.project.prepare_wps_run(self.options.wps_dir)
