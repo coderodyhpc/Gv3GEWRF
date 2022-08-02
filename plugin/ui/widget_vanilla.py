274be86d43fcb8e9e07d421224d2ae5967c108ac
coderodyhpc <79646720+coderodyhpc@users.noreply.github.com>
2022-08-02 11:34:06 -0400

    Update widget_vanilla.py

--- plugin/ui/widget_vanilla.py
+++ plugin/ui/widget_vanilla.py
@@ -50,7 +50,6 @@ class VanillaWidget(QWidget):
 
         self.wps_box, [open_namelist_wps, run_geogrid, run_ungrib, run_metgrid, open_output_wps] = \
             self.create_gbox_with_btns('WPS Preprocessor', [
-                'What to do with this?',
                 'Run Geogrid', 
                 'Run Ungrib', 
                 'Run Metgrid',
@@ -64,9 +63,9 @@ class VanillaWidget(QWidget):
 
         self.setLayout(vbox)
  
-        open_namelist_wps.clicked.connect(self.on_open_namelist_wps_clicked)
+#        open_namelist_wps.clicked.connect(self.on_open_namelist_wps_clicked)
 #        prepare_only_wps.clicked.connect(self.on_prepare_only_wps_clicked)
-#        run_geogrid.clicked.connect(self.on_run_geogrid_clicked)
+        run_geogrid.clicked.connect(self.on_run_geogrid_clicked)
 #        run_ungrib.clicked.connect(self.on_run_ungrib_clicked)
 #        run_metgrid.clicked.connect(self.on_run_metgrid_clicked)
 #        open_output_wps.clicked.connect(self.on_open_output_wps_clicked)
@@ -92,8 +91,97 @@ class VanillaWidget(QWidget):
         gbox.setLayout(vbox)
         return gbox, btns        
 
-    def on_open_namelist_wps_clicked(self) -> None:
-        self.project.update_wps_namelist()
-        self.open_editor_dialog(self.project.wps_namelist_path,
-                                get_namelist_schema('wps'))
+#    def on_open_namelist_wps_clicked(self) -> None:
+#        self.project.update_wps_namelist()
+#        self.open_editor_dialog(self.project.wps_namelist_path,
+#                                get_namelist_schema('wps'))
+
+    def on_run_geogrid_clicked(self) -> None:
+        self.prepare_wps_run()
+
+        # TODO move this into core
+        self.run_program(self.options.geogrid_exe, self.project.run_wps_folder,
+                         supports_mpi=False)
+
+    def prepare_wps_run(self) -> None:
+        if not self.options.wps_dir:
+            raise WPSDistributionError('WPS is not setup')
+        self.project.prepare_wps_run(self.options.wps_dir)
+        
+    def run_program(self, path: str, cwd: str, supports_mpi: bool) -> None:
+        self.dont_report_program_status = False
+        self.wps_box.setVisible(False)
+#        self.wrf_box.setVisible(False)
+#        self.control_box.setVisible(True)
+        try:
+            self.run_program_in_background(path, cwd, self.on_program_execution_done, supports_mpi)
+        except:
+            self.on_program_execution_done(path, None)
+            raise
+
+    def run_program_in_background(self, path: str, cwd: str, on_done: Callable[[str,Union[bool,str,None]],None],
+                                  supports_mpi: bool) -> None:
+        self.stdout_textarea.clear()
+
+        # WRF/WPS does not use exit codes to indicate success/failure,
+        # therefore in addition we look for a pattern in the program output.
+        # TODO add 'FATAL' as patterm
+        wrf_error_pattern = 'ERROR'
+
+        use_mpi = supports_mpi and self.options.mpi_enabled
+        if use_mpi and '-nompi' in path:
+            raise UserError(
+                'MPI is enabled but your WRF/WPS distribution does not support it.\n'
+                'In the plugin options, either choose/download a distribution with MPI support or disable MPI')
+
+        # Using QThread and signals (instead of a plain Python thread) is necessary
+        # so that the on_done callback is run on the UI thread, instead of the worker thread.
+        thread = ProgramThread(path, cwd, wrf_error_pattern,
+                               use_mpi=use_mpi,
+                               mpi_processes=self.options.mpi_processes)
+
+        def on_output(out: str) -> None:
+            self.stdout_textarea.appendPlainText(out)
+            vert_scrollbar = self.stdout_textarea.verticalScrollBar()
+            vert_scrollbar.setValue(vert_scrollbar.maximum())
+
+        def on_finished() -> None:
+            # When lines come in fast, then the highlighter is not called on each line.
+            # Re-highlighting at the end is a work-around to at least have correct
+            # highlighting after program termination.
+            self.stdout_highlighter.rehighlight()
+
+            if thread.exc_info:
+                on_done(path, None)
+                raise thread.exc_info[0].with_traceback(*thread.exc_info[1:])
+            on_done(path, thread.error)
+        thread.output.connect(on_output)
+        thread.finished.connect(on_finished)
+        thread.start()
+        # so that we can kill the program later if requested
+        self.thread = thread
+        
+    def on_program_execution_done(self, path: str, error: Union[bool, str, None]) -> None:
+        self.wps_box.setVisible(True)
+        self.wrf_box.setVisible(True)
+        self.control_box.setVisible(False)
+        # The above causes a resize of the program output textarea
+        # which requires that we scroll to the bottom again.
+        vert_scrollbar = self.stdout_textarea.verticalScrollBar()
+        vert_scrollbar.setValue(vert_scrollbar.maximum())
+
+        if self.dont_report_program_status:
+            return
+        
+        if error is None:
+            # An exception that will be reported by the caller after this function returns.
+            return
+        if error:
+            if isinstance(error, str):
+                raise UserError('Program {} failed: {}'.format(os.path.basename(path), error))
+            else:
+                raise UserError('Program {} failed with errors, check the logs'.format(os.path.basename(path)))
+        else:
+            QMessageBox.information(self.iface.mainWindow(), PLUGIN_NAME, 
+                'Program {} finished without errors!'.format(os.path.basename(path)))            
         
